ece4c6691afd4284b02d05b1aeb7ac18052aa2ec
sandermelles@gmail.com <sandermelles@gmail.com>
2024-10-01 13:55:38 +0300

    Initial commit

---
+++ ShopTARge23/ShopTARge23.Data/ShopTARge23Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge23.Core.Domain;
+//using Microsoft.EntityFrameworkCore.Relational;
+//using Microsoft.Data.SqlClient;
+
+namespace ShopTARge23.Data
+{
+    public class ShopTARge23Context : DbContext
+    {
+        public ShopTARge23Context(DbContextOptions<ShopTARge23Context> options) 
+        : base(options) { }
+
+        public DbSet<Spaceship> Spaceships { get; set; }
+        public DbSet<FileToApi> FileToApi { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //      modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
+        //}
+    }
+}
