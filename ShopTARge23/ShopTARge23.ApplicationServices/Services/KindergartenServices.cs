597a8d2880833d66bd89c4e46b323e7e51bbee3f
sandermelles@gmail.com <sandermelles@gmail.com>
2024-10-28 16:42:03 +0200

    Kindergarten services vol2

--- ShopTARge23/ShopTARge23.ApplicationServices/Services/KindergartenServices.cs
+++ ShopTARge23/ShopTARge23.ApplicationServices/Services/KindergartenServices.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge23.Core.Domain;
+using ShopTARge23.Core.Dto;
+using ShopTARge23.Core.ServiceInterface;
+using ShopTARge23.Data;
+
 
 namespace ShopTARge23.ApplicationServices.Services
 {
-    public class KindergartenServices
+    public class KindergartenServices : IKindergartenServices
     {
+        private readonly ShopTARge23Context _context;
+        private readonly IFileServices _fileServices;
+
+        public KindergartenServices
+            (
+                ShopTARge23Context context,
+                IFileServices fileServices
+            )
+        {
+            _context = context;
+            _fileServices = fileServices;
+        }
+
+        public async Task<Kindergarten> Create(KindergartenDto dto)
+        {
+            Kindergarten kindergarten = new Kindergarten();
+
+            kindergarten.Id = Guid.NewGuid();
+            kindergarten.GroupName = dto.GroupName;
+            kindergarten.ChildrenCount = dto.ChildrenCount;
+            kindergarten.KindergartenName = dto.KindergartenName;
+            kindergarten.Teacher = dto.Teacher;
+            kindergarten.CreatedAt = DateTime.Now;
+            kindergarten.UpdatedAt = DateTime.Now;
+
+            await _context.Kindergartens.AddAsync(kindergarten);
+            await _context.SaveChangesAsync();
+
+            return kindergarten;
+        }
     }
 }
