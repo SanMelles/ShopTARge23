142897ab82802a4d2f9a244ee8f48996760f51f4
sandermelles@gmail.com <sandermelles@gmail.com>
2024-10-28 14:10:38 +0200

    Kindergarten Index

---
+++ ShopTARge23/ShopTARge23/Controllers/KindergartenController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge23.Data;
+using ShopTARge23.Models.Kindergarten;
+
+namespace ShopTARge23.Controllers
+{
+    public class KindergartenController : Controller
+    {
+        private readonly ShopTARge23Context _context;
+        public KindergartenController
+            (
+            ShopTARge23Context context
+            )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Kindergartens
+                .Select(x => new KindergartenIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    KindergartenName = x.KindergartenName,
+                    Teacher = x.Teacher,
+                    CreatedAt = x.CreatedAt,
+                });
+            return View(result);
+        }
+    }
+}
