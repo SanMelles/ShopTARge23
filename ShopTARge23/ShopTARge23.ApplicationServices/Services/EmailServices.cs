b43890c3df7fc4c5b128082529bd3a9046f39131
sandermelles@gmail.com <sandermelles@gmail.com>
2024-11-25 09:44:44 +0200

    AccuWeather and Email init commit

---
+++ ShopTARge23/ShopTARge23.ApplicationServices/Services/EmailServices.cs
@@ -0,0 +1,14 @@
+﻿using ShopTARge23.Core.Dto;
+using MimeKit;
+using ShopTARge23.ApplicationServices.Services;
+
+namespace ShopTARge23.ApplicationServices.Services
+{
+    internal class EmailServices : IEmailServices
+    {
+        public void SendEmail(EmailDto dto)
+        {
+            var email = new MimeMessage();
+        }
+    }
+}
