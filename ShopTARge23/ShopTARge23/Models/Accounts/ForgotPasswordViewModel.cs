ca072e2665962454784a607cfbe2ed83ad51a682
sandermelles@gmail.com <sandermelles@gmail.com>
2024-12-15 21:46:47 +0200

    ForgotPassword

---
+++ ShopTARge23/ShopTARge23/Models/Accounts/ForgotPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ShopTARge23.Models.Accounts
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
